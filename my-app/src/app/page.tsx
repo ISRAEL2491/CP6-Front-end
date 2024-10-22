63d20d44ca8288e71159f3770324c843f942d909
dudabrigidio <dudabrigidio@gmail.com>
2024-10-22 11:56:42 -0300

    Estrutura principal criada

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div>
+            <h1>PORTFOLIO</h1>
+            <p>Este portfólio compila e exibe os resultados das avaliações acadêmicas dos alunos ao longo do curso, proporcionando uma visão clara do desempenho e do progresso em diversas atividades.</p>
+        </div>
+    )
+}
---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <h2>ANÁLISE E DESENVOLVIMENTO DE SISTEMAS</h2>
+            <nav className="info">
+                
+            </nav>
+        </header>
+    )
+}
