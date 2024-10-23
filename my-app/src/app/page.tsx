f02349073953b5b61c35548f6c5dc91a4d26d438
dudabrigidio <dudabrigidio@gmail.com>
2024-10-23 19:31:31 -0300

    css homepage

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,17 +1,27 @@
 import Menu from "@/components/Menu/Menu";
+import Image from "next/image";
 
 
 export default function Home() {
     return (
         <div className="home-page">
-            <main>
+            <Image 
+                src={'/assets/risco-titulo.png'}  
+                alt={'ilustração'} 
+                width={500} 
+                height={2} 
+                className="img-home"
+                />
+            <main className="home-text">
                 <h1>PORTFÓLIO</h1>
                 <p>
                     Este portfólio compila e exibe os resultados das avaliações acadêmicas dos alunos ao longo do curso, 
                     proporcionando uma visão clara do desempenho e do progresso em diversas atividades.
                 </p>
-                <Menu/>
             </main>
+
+            <Menu/>
+
         </div>
     );
 }
