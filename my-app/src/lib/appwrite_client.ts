4031dfbd6d16964ca9752508e1c25478e210cde6
calazans-99 <marcuscalazans01@gmail.com>
2024-10-23 22:16:50 -0300

    att

--- my-app/src/lib/appwrite_client.ts
+++ my-app/src/lib/appwrite_client.ts
@@ -3,5 +3,5 @@ import { Client } from 'appwrite';
 export const client = new Client();
 
 client
-  .setEndpoint('https://cloud.appwrite.io/v1') // Appwrite API endpoint
-  .setProject('6717e4660038656dc9a3'); // Project ID
+  .setEndpoint('https://cloud.appwrite.io/v1') 
+  .setProject('6717e4660038656dc9a3'); 
